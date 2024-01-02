ea7f6df89abf63ff7bcf0daad8bf8605b54e45d9
Michael Hsu <michael100.hsu@gmail.com>
2024-01-02 15:39:11 -0600

    organize comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
-//! TODO: Shows import/no-unresolved error in IDE but not terminal
-import reactLogo from '@assets/react.svg';
 
 /* eslint-disable import/no-unresolved */
+import reactLogo from '@assets/react.svg'; //! TODO: Shows import/no-unresolved error in IDE but not terminal
 import viteLogo from '/vite.svg'; // TODO: Figure out how to use public paths with ESLint
 /* eslint-enable import/no-unresolved */
 
