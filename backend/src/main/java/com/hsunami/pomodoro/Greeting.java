fb2077e27e27f59890d580e72c5af2502c2091a7
Michael Hsu <michael100.hsu@gmail.com>
2024-01-10 15:47:50 -0600

    init spring boot server

---
+++ backend/src/main/java/com/hsunami/pomodoro/Greeting.java
@@ -0,0 +1,3 @@
+package com.hsunami.pomodoro;
+
+public record Greeting(long id, String content) {}
---
+++ backend/src/main/java/com/hsunami/pomodoro/HelloController.java
@@ -0,0 +1,13 @@
+package com.hsunami.pomodoro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "This is the index controller from Spring Boot!";
+    }
+}
