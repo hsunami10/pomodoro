4132914306bb1e9964e1dee7fe3b1f581deba2a7
Michael Hsu <michael100.hsu@gmail.com>
2024-01-12 21:47:26 -0600

    add escape key to close modal

--- frontend/src/components/modal/Modal.tsx
+++ frontend/src/components/modal/Modal.tsx
@@ -1,23 +1,47 @@
-import { ReactNode } from 'react';
+import { ReactNode, useCallback, useEffect } from 'react';
 import styles from './Modal.module.css';
 
 export interface ModalProps {
   isOpen: boolean;
-  hasCloseBtn?: boolean;
+  hasCloseButton?: boolean;
   onClose: () => void;
   children?: ReactNode;
 }
 
-const Modal = ({ isOpen, hasCloseBtn = true, onClose, children }: ModalProps) =>
-  isOpen && (
-    <dialog className={styles.container}>
-      <div className={styles.header}>
-        {hasCloseBtn && (
-          <button onClick={() => onClose && onClose()}>Close</button>
-        )}
-      </div>
-      {children}
-    </dialog>
+const Modal = ({
+  isOpen,
+  hasCloseButton = true,
+  onClose,
+  children,
+}: ModalProps) => {
+  const handleEscapeKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose && onClose();
+      }
+    },
+    [onClose],
   );
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleEscapeKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleEscapeKeyDown);
+    };
+  }, [handleEscapeKeyDown]);
+
+  return (
+    isOpen && (
+      <dialog className={styles.container}>
+        {hasCloseButton && (
+          <div className={styles.header}>
+            <button onClick={() => onClose && onClose()}>Close</button>
+          </div>
+        )}
+        {children}
+      </dialog>
+    )
+  );
+};
 
 export default Modal;
